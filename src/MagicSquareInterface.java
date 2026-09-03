111b2e4df1410748db9b12f4f081105ca59b2a7b
ced4rtree <cedar@cebar.xyz>
2026-09-02 19:44:32 -0600

    yucky tabs replaced with beautiful spaces

--- src/MagicSquareInterface.java
+++ src/MagicSquareInterface.java
@@ -38,39 +38,39 @@
  * @author mvail
  */
 public interface MagicSquareInterface {
-	/**
-	 * Evaluate the matrix (whether read from file or
-	 * generated) and return a boolean verdict of
-	 * whether the matrix is a magic square.
-	 * 
-	 * @return true if matrix is a magic square, else false
-	 */
-	public boolean isMagicSquare();
-	
-	/**
-	 * Return a copy of the matrix (whether read from file 
-	 * or generated) as a 2D array of ints. Be sure this
-	 * method does not compromise encapsulation.
-	 * 
-	 * @return 2D array of ints that may or may not be a valid magic square
-	 */
-	public int[][] getMatrix();
-	
-	/**
-	 * Return a string formatted as in these examples:
-	 *   The matrix
-	 *     4 9 2
-	 *     3 5 7
-	 *     8 1 6
-	 *   is a magic square.
-	 *  or
-	 *    The matrix
-	 *      5 3 7
-	 *      9 4 2
-	 *      1 6 5
-	 *    is not a magic square.
-	 *    
-	 * @return formatted string showing the matrix and whether it is a valid magic square
-	 */
-	public String toString();
+    /**
+     * Evaluate the matrix (whether read from file or
+     * generated) and return a boolean verdict of
+     * whether the matrix is a magic square.
+     * 
+     * @return true if matrix is a magic square, else false
+     */
+    public boolean isMagicSquare();
+    
+    /**
+     * Return a copy of the matrix (whether read from file 
+     * or generated) as a 2D array of ints. Be sure this
+     * method does not compromise encapsulation.
+     * 
+     * @return 2D array of ints that may or may not be a valid magic square
+     */
+    public int[][] getMatrix();
+    
+    /**
+     * Return a string formatted as in these examples:
+     *   The matrix
+     *     4 9 2
+     *     3 5 7
+     *     8 1 6
+     *   is a magic square.
+     *  or
+     *    The matrix
+     *      5 3 7
+     *      9 4 2
+     *      1 6 5
+     *    is not a magic square.
+     *    
+     * @return formatted string showing the matrix and whether it is a valid magic square
+     */
+    public String toString();
 }
